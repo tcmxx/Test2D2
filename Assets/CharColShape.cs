1c1e29b8fdbf2eea7e7e1015a481d560c66bd30c
tcmxx <maxiaoxiao1991@gmail.com>
2014-09-12 14:29:29 -0400

    2D test for SuperC

---
+++ Assets/CharColShape.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharColShape : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D( Collision2D coll ) {                               
+		// Find out what hit this basket
+		GameObject collidedWith = coll.gameObject;
+		if ( collidedWith.tag == "Touchable" ) {                                
+			foreach (var contact in coll.contacts) {
+				// Visualize the contact point
+				print(contact.collider + " hit " + contact.otherCollider.name);
+				Debug.DrawRay(contact.point, contact.normal, Color.white);
+			}
+		}
+	}
+}
---
+++ Assets/CharMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CharMovement : MonoBehaviour {
+	public float MaxHorSpeed = 1f;
+	public float MaxVerSpeed = 1f;
+	public float g = -9.8f;
+
+	public bool ________________;
+	public enum CharStatus{STAND, RUN, JUMP, FALL};
+	public float horV = 0f;
+	public float vertV = 0f;
+	public CharStatus status = CharStatus.STAND;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+
+		if (Input.GetKey (KeyCode.W)) {
+
+		} else if (Input.GetKey (KeyCode.S)) {
+
+		}else if (Input.GetKey (KeyCode.A)) {
+			horV = -MaxHorSpeed;
+		}else if (Input.GetKey (KeyCode.D)) {
+			horV = MaxHorSpeed;
+		}else if (Input.GetKey (KeyCode.J)) {
+		}else if (Input.GetKey (KeyCode.K)) {
+			if(status == CharStatus.STAND || status == CharStatus.RUN){
+				vertV = MaxVerSpeed;
+				status = CharStatus.JUMP;
+			}
+		}else{
+			horV = 0;
+		}
+
+
+
+
+		pos.x += horV * Time.deltaTime;
+		pos.y += vertV * Time.deltaTime;
+		transform.position = pos;
+
+		if (status == CharStatus.JUMP || status == CharStatus.FALL) {
+			vertV += g * Time.deltaTime;
+		}
+
+
+	}
+
+	
+
+
+}
